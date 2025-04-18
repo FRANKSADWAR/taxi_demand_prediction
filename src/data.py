7490ba2158bcd5b20c563bdde5267be509a8ab8a
FRANKSADWAR <billyfranks98@gmail.com>
2025-04-18 13:07:50 +0300

    function to filter rides by their dates

--- src/data.py
+++ src/data.py
@@ -23,4 +23,8 @@ def validate_raw_data(rides: pd.DataFrame, year: int, month: int) -> pd.DataFram
     Removes the rows with pickup dates outside their valid range
     """
     this_month_start = f'{year}-{month:02d}-01'
-    next_month_start = f'{year}-{month+1:02d}-01' if month < 12 else f'{year}'
+    next_month_start = f'{year}-{month+1:02d}-01' if month < 12 else f'{year+1}-01-01'
+    rides = rides[rides.pickup_datetime >= this_month_start]
+    rides = rides[rides.pickup_datetime < next_month_start]
+
+    return rides
